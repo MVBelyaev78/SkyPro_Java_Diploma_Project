85bd994bb8d9ae45c7b0f014333a3c675ec8e0b0
79774 <Cinnabon_09@.com>
2025-10-09 16:34:21 +0300

    -

--- src/main/java/ru/skypro/homework/dto/Login.java
+++ src/main/java/ru/skypro/homework/dto/Login.java
@@ -1,10 +1,18 @@
 package ru.skypro.homework.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+/**
+ * DTO для аутентификации пользователя.
+ */
 @Data
+@Schema(description = "Объект для аутентификации пользователя")
 public class Login {
 
+    @Schema(description = "Имя пользователя", example = "user123", required = true)
     private String username;
+
+    @Schema(description = "Пароль пользователя", example = "password123", required = true)
     private String password;
 }
--- src/main/java/ru/skypro/homework/dto/Register.java
+++ src/main/java/ru/skypro/homework/dto/Register.java
@@ -1,14 +1,29 @@
 package ru.skypro.homework.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+/**
+ * Класс, представляющий объект регистрации пользователя.
+ */
 @Data
 public class Register {
 
+    @Schema(description = "Имя пользователя", example = "john_doe")
     private String username;
+
+    @Schema(description = "Пароль пользователя", example = "securePassword123")
     private String password;
+
+    @Schema(description = "Имя пользователя", example = "John")
     private String firstName;
+
+    @Schema(description = "Фамилия пользователя", example = "Doe")
     private String lastName;
+
+    @Schema(description = "Телефонный номер пользователя", example = "+1234567890")
     private String phone;
+
+    @Schema(description = "Роль пользователя", example = "USER")
     private Role role;
 }
