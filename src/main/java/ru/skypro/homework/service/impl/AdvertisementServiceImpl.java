595e3c0ec790157a2c2ac4dcf12b67b927079e79
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-10-10 17:28:02 +0300

    updateAdvertisementInfo added

--- src/main/java/ru/skypro/homework/service/impl/AdvertisementServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AdvertisementServiceImpl.java
@@ -3,6 +3,7 @@ package ru.skypro.homework.service.impl;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.Ad;
 import ru.skypro.homework.dto.Ads;
+import ru.skypro.homework.dto.CreateOrUpdateAd;
 import ru.skypro.homework.dto.ExtendedAd;
 import ru.skypro.homework.service.AdvertisementService;
 
@@ -28,5 +29,10 @@ public class AdvertisementServiceImpl implements AdvertisementService {
     @Override
     public Boolean deleteAdvertisement(Long id) {
         return true;
+    }
+
+    @Override
+    public Ad updateAdvertisementInfo(Long id, CreateOrUpdateAd createOrUpdateAd) {
+        return new Ad(0L, "string", "string", createOrUpdateAd.getPrice(), createOrUpdateAd.getTitle());
     }
 }
