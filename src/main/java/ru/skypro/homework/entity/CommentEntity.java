45b2b880ce37de5a4fae7e6131fb62aa4c5ec2f1
MaximCh <bezdomnyi1997@gmail.com>
2025-10-10 19:56:52 +0500

    [#34] added comment entity

--- src/main/java/ru/skypro/homework/entity/CommentEntity.java
+++ src/main/java/ru/skypro/homework/entity/CommentEntity.java
@@ -3,6 +3,9 @@ package ru.skypro.homework.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.skypro.homework.dto.User;
+
+import javax.persistence.*;
 
 
 /**
@@ -12,37 +15,42 @@ import lombok.NoArgsConstructor;
  * @version 1.0
  */
 
+
+@Entity
+@Table(name = "tbl_comment", schema = "public")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentEntity {
-    /**
-     * Идентификатор автора комментария
-     */
-    private int author;
 
-    /**
-     * Ссылка на аватарку автора
-     */
-    private String authorImage;
-
-    /**
-     * Имя автора
-     */
-    private String authorFirstName;
-
-    /**
-     * Время создания комментария
-     */
-    private int createAt;
-
-    /**
-     * Идентификатор комментария
-     */
-    private int pk;
+    @Id
+    @GeneratedValue
+    @Column(name = "id_comment")
+    private int idComment;
 
     /**
      * Текст комментария
      */
-    private String text;
+    @Column(name = "nm_text", nullable = false, length = 200)
+    private String nmText;
+
+    /**
+     * Время создания комментария
+     */
+    @Column(name = "dt_create")
+    private int dtCreate;
+
+    /**
+     * Идентификатор объявления
+     */
+    @ManyToOne
+    @JoinColumn(name = "id_advertisement", nullable = false)
+    private AdvertisementEntity idAdvertisement;
+
+    /**
+     * Идентификатор автора комментария
+     */
+    @ManyToOne
+    @JoinColumn(name = "id_author", nullable = false)
+    private UserEntity idAuthor;
 }
