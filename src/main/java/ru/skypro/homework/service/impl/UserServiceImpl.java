79fd5b543bcdbc4b19e5772471de86d44a8b889f
MaximCh <bezdomnyi1997@gmail.com>
2025-10-19 00:50:51 +0500

    [#99] changing user password

--- src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
@@ -1,18 +1,37 @@
 package ru.skypro.homework.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.Role;
 import ru.skypro.homework.dto.UpdateUser;
 import ru.skypro.homework.dto.User;
+import ru.skypro.homework.entity.UserEntity;
+import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.service.UserService;
 
 import java.io.IOException;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public boolean changePassword(String userName, String currentPassword, String newPassword) {
+        UserEntity user = userRepository.findByEmail(userName)
+                .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
+
+        if (!passwordEncoder.matches(currentPassword, user.getPassword())) {
+            return false;
+        }
+
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
         return true;
     }
 
