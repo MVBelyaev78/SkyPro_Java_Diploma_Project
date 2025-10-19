8b063233e6ecf5adbb2a378f8ca42414ea4ac1bc
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-10-20 02:10:15 +0300

    AuthorizationService

--- src/main/java/ru/skypro/homework/service/AuthorizationService.java
+++ src/main/java/ru/skypro/homework/service/AuthorizationService.java
@@ -1,49 +1,11 @@
 package ru.skypro.homework.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import ru.skypro.homework.entity.AdvertisementEntity;
-import ru.skypro.homework.entity.CommentEntity;
-import ru.skypro.homework.entity.UserEntity;
-import ru.skypro.homework.repository.AdvertisementRepository;
-import ru.skypro.homework.repository.CommentRepository;
+public interface AuthorizationService {
+    boolean isCommentAuthor(int commentId);
 
-@Service
-@RequiredArgsConstructor
-public class AuthorizationService {
-    private final CommentRepository commentRepository;
-    private final AdvertisementRepository advertisementRepository;
-    private final CurrentUserService currentUserService;
+    boolean isAdAuthor(Long adId);
 
-    public boolean isCommentAuthor(int commentId) {
-        CommentEntity comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new RuntimeException("Комментарий не найден"));
+    boolean isCommentAuthorOrAdmin(int commentId);
 
-        UserEntity currentUser = currentUserService.getCurrentUser();
-        return comment.getIdAuthor().getId().equals(currentUser.getId());
-    }
-
-    public boolean isAdAuthor(Long adId) {
-        AdvertisementEntity ad = advertisementRepository.findById(adId)
-                .orElseThrow(() -> new RuntimeException("Объявление не найдено"));
-
-        UserEntity currentUser = currentUserService.getCurrentUser();
-        return ad.getUser().getId().equals(currentUser.getId());
-    }
-
-    public boolean isCommentAuthorOrAdmin(int commentId) {
-        if (currentUserService.isCurrentUserAdmin()) {
-            return true;
-        }
-
-        return isCommentAuthor(commentId);
-    }
-
-    public boolean isAdAuthorOrAdmin(Long adId) {
-        if (currentUserService.isCurrentUserAdmin()) {
-            return true;
-        }
-
-        return isAdAuthor(adId);
-    }
+    boolean isAdAuthorOrAdmin(Long adId);
 }
---
+++ src/main/java/ru/skypro/homework/service/impl/AuthorizationServiceImpl.java
@@ -0,0 +1,51 @@
+package ru.skypro.homework.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.skypro.homework.entity.AdvertisementEntity;
+import ru.skypro.homework.entity.CommentEntity;
+import ru.skypro.homework.entity.UserEntity;
+import ru.skypro.homework.repository.AdvertisementRepository;
+import ru.skypro.homework.repository.CommentRepository;
+import ru.skypro.homework.service.AuthorizationService;
+import ru.skypro.homework.service.CurrentUserService;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorizationServiceImpl implements AuthorizationService {
+    private final CommentRepository commentRepository;
+    private final AdvertisementRepository advertisementRepository;
+    private final CurrentUserService currentUserService;
+
+    public boolean isCommentAuthor(int commentId) {
+        CommentEntity comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new RuntimeException("Комментарий не найден"));
+
+        UserEntity currentUser = currentUserService.getCurrentUser();
+        return comment.getIdAuthor().getId().equals(currentUser.getId());
+    }
+
+    public boolean isAdAuthor(Long adId) {
+        AdvertisementEntity ad = advertisementRepository.findById(adId)
+                .orElseThrow(() -> new RuntimeException("Объявление не найдено"));
+
+        UserEntity currentUser = currentUserService.getCurrentUser();
+        return ad.getUser().getId().equals(currentUser.getId());
+    }
+
+    public boolean isCommentAuthorOrAdmin(int commentId) {
+        if (currentUserService.isCurrentUserAdmin()) {
+            return true;
+        }
+
+        return isCommentAuthor(commentId);
+    }
+
+    public boolean isAdAuthorOrAdmin(Long adId) {
+        if (currentUserService.isCurrentUserAdmin()) {
+            return true;
+        }
+
+        return isAdAuthor(adId);
+    }
+}
