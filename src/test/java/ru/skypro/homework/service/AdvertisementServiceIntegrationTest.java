9d40921962a7f3c998367f53e6fe2c1b08552531
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-10-21 23:01:23 +0300

    in progress

--- src/test/java/ru/skypro/homework/service/AdvertisementServiceIntegrationTest.java
+++ src/test/java/ru/skypro/homework/service/AdvertisementServiceIntegrationTest.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-public class AdvertisementServiceTest {
+public class AdvertisementServiceIntegrationTest {
     @Autowired
     private AdvertisementService advertisementService;
 
