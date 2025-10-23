9bd53660eedecb5f20668fd8cd859dd1e77ac97f
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-10-23 20:50:39 +0300

    in progress

--- src/main/java/ru/skypro/homework/entity/CommentEntity.java
+++ src/main/java/ru/skypro/homework/entity/CommentEntity.java
@@ -1,9 +1,7 @@
 package ru.skypro.homework.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.Instant;
@@ -25,6 +23,8 @@ import java.time.Instant;
 public class CommentEntity {
 
     @Id
+    @ToString.Include
+    @EqualsAndHashCode.Include
     @GeneratedValue
     @Column(name = "id_comment")
     private Integer idComment;
