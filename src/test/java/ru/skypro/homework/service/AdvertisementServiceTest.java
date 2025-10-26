f3e0d841453fbc7cf92ee4ae3f15ddad7dc28c2f
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-10-26 14:18:56 +0300

    complete

--- src/test/java/ru/skypro/homework/service/AdvertisementServiceTest.java
+++ src/test/java/ru/skypro/homework/service/AdvertisementServiceTest.java
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class AdvertisementServiceUnitTest {
+public class AdvertisementServiceTest {
     @Mock
     private AdvertisementRepository repository;
 
