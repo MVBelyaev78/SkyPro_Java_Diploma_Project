73042ddb482bdb1c52f42499b0c19eebaa85dab5
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-10-26 14:32:31 +0300

    complete

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skypro.homework.dto.Login;
-import ru.skypro.homework.dto.Register;
 import ru.skypro.homework.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
