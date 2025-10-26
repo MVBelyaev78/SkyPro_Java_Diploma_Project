9e2b2d39e4333cc29ec42d21b58fbe727c90d536
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-10-26 15:24:58 +0300

    in progress

--- src/main/java/ru/skypro/homework/service/impl/UserEntityDetailsServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/UserEntityDetailsServiceImpl.java
@@ -19,7 +19,7 @@ import ru.skypro.homework.repository.UserRepository;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UserEntityDetailsService implements UserDetailsService {
+public class UserEntityDetailsServiceImpl implements UserDetailsService {
     private final UserRepository repository;
 
     /**
