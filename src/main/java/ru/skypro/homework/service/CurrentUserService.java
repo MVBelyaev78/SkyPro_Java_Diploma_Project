a2e6d2a48f12201d3cc5754c469ad6cd6391030e
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-10-26 15:11:11 +0300

    in progress

--- src/main/java/ru/skypro/homework/service/CurrentUserService.java
+++
@@ -1,9 +0,0 @@
-package ru.skypro.homework.service;
-
-import ru.skypro.homework.entity.UserEntity;
-
-public interface CurrentUserService {
-    UserEntity getCurrentUser();
-    Long getCurrentUserId();
-    boolean isCurrentUserAdmin();
-}
