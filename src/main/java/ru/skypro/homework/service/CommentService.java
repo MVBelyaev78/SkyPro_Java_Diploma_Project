45d73124140cf9296b54465e62239f3ee333910e
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-10-29 00:58:02 +0300

    in progress

--- src/main/java/ru/skypro/homework/service/CommentService.java
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -3,9 +3,6 @@ package ru.skypro.homework.service;
 import ru.skypro.homework.dto.Comment;
 import ru.skypro.homework.dto.Comments;
 import ru.skypro.homework.dto.CreateOrUpdateComment;
-import ru.skypro.homework.entity.AdvertisementEntity;
-import ru.skypro.homework.entity.CommentEntity;
-import ru.skypro.homework.entity.UserEntity;
 
 import java.time.ZonedDateTime;
 import java.util.Optional;
