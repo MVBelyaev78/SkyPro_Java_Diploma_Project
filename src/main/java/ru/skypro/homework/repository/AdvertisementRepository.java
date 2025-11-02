39d21c2a822b5cfb33bf6b173b29a1e56351a321
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-11-02 22:08:59 +0300

    complete

--- src/main/java/ru/skypro/homework/repository/AdvertisementRepository.java
+++ src/main/java/ru/skypro/homework/repository/AdvertisementRepository.java
@@ -1,11 +1,13 @@
 package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.AdvertisementEntity;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AdvertisementRepository extends JpaRepository<AdvertisementEntity, Long> {
     /**
      * Поиск объяввления по ID объявления
